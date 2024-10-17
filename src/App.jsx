cfff6c3bd3ccfe334317e5d2190d0d7266c39002
Ulugbek-Raxmatillayev <ulugbekraxmatillayev615@gmail.com>
2024-10-17 21:52:58 +0500

    update 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,3 @@
-// src/App.js
-import React, { useState } from 'react';
-import emailjs from 'emailjs-com';
-import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Notfound from './notfound';
 import { Route, Routes } from 'react-router-dom';
